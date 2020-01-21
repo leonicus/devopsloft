23162ff540935191b3767a60e8ba2cd6524a9f95
ldobrzinsky <ldobrzinsky@paloaltonetworks.com>
2020-01-21 05:54:18 +0200

    Adds Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = "us-east-2"
+  }
+  resource "aws_instance" "app" {
+  ami = "ami-0e55e373"
+  instance_type = "t1.micro"
+  key_name = "${var.key_name}"
+  }
+  resource "aws_instance" "db" {
+  ami = "ami-0e55e373"
+  instance_type = "t1.micro"
+  key_name = "${var.key_name}"
+  }
+  resource "aws_instance" "vault" {
+  ami = "ami-0e55e373"
+  instance_type = "t1.micro"
+  key_name = "${var.key_name}"
+ }
+
+resource "aws_security_group" "default" {
+  name = "sg_staging"
+  # Allow all outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound HTTP from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+	
+}
